df52d557eac6c872198f480f4f980e532c2e495a
Kelvin Mwangemi <39115419+kmwangemi@users.noreply.github.com>
2018-10-23 14:55:54 +0300

    #161214626 added api endpoint for creating a product (#18)
    * [Feature #161214626] added api endpoint for creating a product
    
    * modified .travis.yml
    
    * modified .travis.yml
    
    * modified travis and requirements files
    
    * modified travis and requirements files
    
    * [Feature #161214626] adding a function for creating product

---
+++ tests/V1/test_product_view.py
@@ -0,0 +1,48 @@
+import unittest
+import json
+from run import app
+from app.api.V1.views.product_views import product_info
+
+#import os
+
+class ProductstestCase(unittest.TestCase):
+
+    def setUp(self):
+        """will be called before every test"""
+        self.client = app.test_client
+
+        self.product = {
+                        "product_name" : "product_name",
+                        "category" : "category",
+                        "quantity" : "quantity",
+                        "price" : "price",
+                        "description" : "description"
+                        }
+
+        self.empty_product = {
+                                "product_name" : "",
+                                "category" : "",
+                                "quantity" : "",
+                                "price" : "",
+                                "description" : ""
+                            }
+
+    '''Tests for product creation'''
+    def test_product_created_successfully(self):
+        """Tests that a product is created successfully"""
+        res = self.client().post('/api/v1/products', data=json.dumps(self.product), content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        self.assertIn("Product created", str(res.data))
+    
+    def test_product_cannot_create_with_invalid_details(self):
+        """Tests that a product cannot be created with empty fields"""
+        res = self.client().post('/api/v1/products', data=json.dumps(self.empty_product), content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+
+    
+
+   
+
+    
+  
+    
