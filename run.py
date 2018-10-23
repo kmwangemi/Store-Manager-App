cc95c4c3fcdc26ac4b887bc187f135d2f7383bc7
kmwangemi <mwangemik@gmail.com>
2018-10-23 22:46:25 +0300

    modifying run.py file

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from app import create_app
 
 
-config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+config_name = "development"
 app = create_app(config_name)
 # method to run the flask app
 if __name__ == '__main__':
